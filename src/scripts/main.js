59e04a8c3f4ab3ad9cb23a8295338a2bf1257fe9
IvanSostarko <ivan.sostarko@hotmail.com>
2016-08-29 04:02:09 +0200

    homepage portfolio

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,12 +14,13 @@ $(document).ready(function () {
     caption();
     waypoints();
     showToolTip();
-
+    openPortfolioModal();
+    carousel_project_type_website();
 
 
     //Page loader
     function loader() {
-        $(window).bind("load", function() {
+        $(window).bind("load", function () {
             $('#wrapper').addClass('loaded');
         });
     }
@@ -48,7 +49,7 @@ $(document).ready(function () {
 
     //Scroll to about page
     function scrollToAbout() {
-        $("#scrollToAbout").click(function (){
+        $("#scrollToAbout").click(function () {
             $('html, body').animate({
                 scrollTop: $("#about").offset().top
             }, 1000);
@@ -92,10 +93,10 @@ $(document).ready(function () {
     //Caption
     function caption() {
         $('.thumbnail').hover(
-            function(){
+            function () {
                 $(this).find('.caption').slideDown(250); //.fadeIn(250)
             },
-            function(){
+            function () {
                 $(this).find('.caption').slideUp(250); //.fadeOut(205)
             }
         );
@@ -105,14 +106,14 @@ $(document).ready(function () {
 
     //Waypoints animations
     function waypoints() {
-        $('.animate_waypoint').waypoint(function() {
+        $('.animate_waypoint').waypoint(function () {
             $('.animate_waypoint').addClass('fadeInDown');
-        }, { offset: '100%' });
+        }, {offset: '100%'});
     }
 
 
     //Custom modal
-    function customModal(){
+    function customModal() {
         /**
          * modalEffects.js v1.0.0
          * http://www.codrops.com
@@ -123,47 +124,47 @@ $(document).ready(function () {
          * Copyright 2013, Codrops
          * http://www.codrops.com
          */
-        var ModalEffects = (function() {
+        var ModalEffects = (function () {
 
             function init() {
 
-                var overlay = document.querySelector( '.md-overlay' );
+                var overlay = document.querySelector('.md-overlay');
 
-                [].slice.call( document.querySelectorAll( '.md-trigger' ) ).forEach( function( el, i ) {
+                [].slice.call(document.querySelectorAll('.md-trigger')).forEach(function (el, i) {
 
-                    var modal = document.querySelector( '#' + el.getAttribute( 'data-modal' ) ),
-                        close = modal.querySelector( '.md-close' );
+                    var modal = document.querySelector('#' + el.getAttribute('data-modal')),
+                        close = modal.querySelector('.md-close');
 
-                    function removeModal( hasPerspective ) {
-                        classie.remove( modal, 'md-show' );
+                    function removeModal(hasPerspective) {
+                        classie.remove(modal, 'md-show');
 
-                        if( hasPerspective ) {
-                            classie.remove( document.documentElement, 'md-perspective' );
+                        if (hasPerspective) {
+                            classie.remove(document.documentElement, 'md-perspective');
                         }
                     }
 
                     function removeModalHandler() {
-                        removeModal( classie.has( el, 'md-setperspective' ) );
+                        removeModal(classie.has(el, 'md-setperspective'));
                     }
 
-                    el.addEventListener( 'click', function( ev ) {
-                        classie.add( modal, 'md-show' );
-                        overlay.removeEventListener( 'click', removeModalHandler );
-                        overlay.addEventListener( 'click', removeModalHandler );
+                    el.addEventListener('click', function (ev) {
+                        classie.add(modal, 'md-show');
+                        overlay.removeEventListener('click', removeModalHandler);
+                        overlay.addEventListener('click', removeModalHandler);
 
-                        if( classie.has( el, 'md-setperspective' ) ) {
-                            setTimeout( function() {
-                                classie.add( document.documentElement, 'md-perspective' );
-                            }, 25 );
+                        if (classie.has(el, 'md-setperspective')) {
+                            setTimeout(function () {
+                                classie.add(document.documentElement, 'md-perspective');
+                            }, 25);
                         }
                     });
 
-                    close.addEventListener( 'click', function( ev ) {
+                    close.addEventListener('click', function (ev) {
                         ev.stopPropagation();
                         removeModalHandler();
                     });
 
-                } );
+                });
 
             }
 
@@ -172,4 +173,98 @@ $(document).ready(function () {
         })();
     }
 
-})
+    function openPortfolioModal() {
+        var modalTrigger = $(".morph-btn");
+        var modalWindow = $(".morph-modal");
+        var closeTrigger = $(".close-modal");
+
+
+        function getElementPosition(getSelectedModalTrigger) {
+            var top = getSelectedModalTrigger.offset().top - $(window).scrollTop();
+            var left = getSelectedModalTrigger.offset().left;
+            //return an array
+            return [top, left];
+        }
+
+
+        function evalScale(element, position) {
+            var scaleY = scaleValue(position[0], element.innerHeight(), $(window).height());
+            var scaleX = scaleValue(position[1], element.innerWidth(), $(window).width());
+
+            return [scaleY, scaleX];
+        }
+
+
+        function scaleValue(firstCoord, elSize, windowSize) {
+            var secondCoord = windowSize - firstCoord - elSize; //bottom
+            var maxCoord = Math.max(firstCoord, secondCoord);
+            var scaleValue = (maxCoord * 2 + elSize) / elSize; //final size of the span element
+            return Math.ceil(scaleValue * 10) / 10;
+        }
+
+        function launchModal(e) {
+            e.preventDefault
+
+            //hide scroll body
+            $('body').css('overflow', 'hidden');
+            $('#go_top').hide();
+
+            var selectedModalTrigger = $(this);
+            //get href
+            var modalId = selectedModalTrigger.attr("href");
+
+            var selectedModalWindow = modalWindow.filter(modalId);
+            var selectedMorphBackground = selectedModalWindow.children(".morph-background");
+            selectedModalWindow.addClass("open-modal");
+
+            //run trigger button position function (pass in the element)
+            var triggerPosition = getElementPosition(selectedModalTrigger);
+
+            var scaleValues = evalScale(selectedMorphBackground, triggerPosition);
+
+            //use array index to locate position
+            selectedMorphBackground.css({
+                "top": triggerPosition[0] + "px",
+                "left": triggerPosition[1] + "px",
+                "transform": "scaleX(" + scaleValues[1] + ") scaleY(" + scaleValues[0] + ")",
+                //"border":"1px solid red"
+            }).one("transitionend", function () {
+                //one works like on, but listens to the callback only once
+                selectedModalWindow.addClass("modal-visible");
+            });
+
+            // function showContent(){
+            //   selectedModalWindow.addClass("modal-visible");
+            // }
+        }
+
+        modalTrigger.on("click", launchModal);
+
+
+        modalWindow.on("click", ".close-modal", closeModal);
+        function closeModal(event) {
+            //show scroll on body
+            $('body').css('overflow', 'scroll');
+            $('#go_top').show();
+
+
+            //get modal window && background
+            var selectedModalWindow = $(this).parent(".morph-modal");
+            var selectedBackground = selectedModalWindow.children(".morph-background");
+
+            //hide the modal content
+            modalWindow.removeClass("modal-visible");
+            //reduce
+            selectedBackground.css({
+                "transform": "scaleX(1) scaleY(1)"
+            }).one("transitionend", function () {
+                selectedModalWindow.removeClass("open-modal");
+            });
+        }
+    }
+
+    function carousel_project_type_website() {
+        $('#carousel_project_type_website').carousel()
+    }
+
+});
