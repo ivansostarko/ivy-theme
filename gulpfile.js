86bf6116165f3d234ed2fdd410efc9ebfcd8de28
IvanSostarko <ivan.sostarko@hotmail.com>
2016-08-30 23:06:56 +0200

    travis

--- gulpfile.js
+++ gulpfile.js
@@ -62,6 +62,12 @@ gulp.task('scripts', function(){
 });
 
 
+gulp.task('travis', ['bs-reload', 'images', 'styles', 'scripts'], function(){
+    gulp.watch("src/scss/**/*.scss", ['styles', 'styles']);
+    gulp.watch("src/scripts/**/*.js", ['scripts']);
+    gulp.watch("*.html", ['bs-reload']);
+});
+
 gulp.task('default', ['browser-sync', 'bs-reload', 'images', 'styles', 'scripts'], function(){
     gulp.watch("src/scss/**/*.scss", ['styles', 'styles']);
     gulp.watch("src/scripts/**/*.js", ['scripts']);
